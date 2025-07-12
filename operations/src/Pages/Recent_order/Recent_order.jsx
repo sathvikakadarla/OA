825893abcf799ff3ea10a6d407f321a11bf4e440
sathvikakadarla <133248594+sathvikakadarla@users.noreply.github.com>
2025-07-12 15:54:11 +0530

    Update Recent_order.jsx

--- operations/src/Pages/Recent_order/Recent_order.jsx
+++ operations/src/Pages/Recent_order/Recent_order.jsx
@@ -2,7 +2,36 @@ import React from "react";
 import './Recent_order.css'
 
 const Recentorders = () => {
+    return (import React from "react";
+import './Recent_order.css'
+
+const Recentorders = () => {
     return (
+      <div className="order-container">
+      <div className="recent-orders">
+      <h2>Recent Orders</h2>
+      <table className="orders-table">
+        <thead>
+          <tr>
+            <th>Order ID</th>
+            <th>Customer</th>
+            <th>Status</th>
+            <th>Scheduled For</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+      <div className="delivery-container">
+        <div className="delivery-heading">
+        <h2>Delivery Partner</h2>
+        </div>
+      </div>
+    </div>
+    )
+}
+
+export default Recentorders;
       <div className="order-container">
       <div className="recent-orders">
       <h2>Recent Orders</h2>
