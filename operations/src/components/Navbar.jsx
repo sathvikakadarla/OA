1e08673d0d12877596cccabac32874a325574bf9
sathvikakadarla <133248594+sathvikakadarla@users.noreply.github.com>
2025-07-12 15:50:45 +0530

    Update Navbar.jsx

--- operations/src/components/Navbar.jsx
+++ operations/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Navbar.css';
 import { assets } from '../assets/assets';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { useAuth } from '../Pages/AuthContext/AuthContext';
 import OperationalBanner from './Operationalbanner';
 import Login from '../Pages/Login/Login';
@@ -9,11 +9,23 @@ import Login from '../Pages/Login/Login';
 const Navbar = () => {
   const { isLoggedIn, setIsLoggedIn } = useAuth();
   const location = useLocation();
+  const navigate = useNavigate();
+
   const [showLogin, setShowLogin] = useState(false);
+  const [hasLoggedOut, setHasLoggedOut] = useState(false);
 
   const handleLogout = () => {
     setIsLoggedIn(false);
+    setShowLogin(false);
+    setHasLoggedOut(true);
+    navigate('/');
   };
+
+  useEffect(() => {
+    if (location.pathname === '/' && hasLoggedOut) {
+      setHasLoggedOut(false);
+    }
+  }, [location.pathname, hasLoggedOut]);
 
   const handleLoginClick = () => {
     setShowLogin(true);
@@ -23,17 +35,21 @@ const Navbar = () => {
     setShowLogin(false);
   };
 
-  // Don't show navbar on actual /login route
+  // Don't show navbar on /login route
   if (location.pathname === '/login') return null;
 
   return (
     <>
       <div className="navbar-header">
         <div className="navbar-logo">
-          <Link to="/" className="logo">
-            <img className="logo-img" src={assets.noveglogo} alt="Logo" />
-          </Link>
+          <img className="logo-img" src={assets.NoVegRapixLogo} alt="Logo" />
         </div>
+
+        {isLoggedIn && (
+          <div className="home-div">
+            <button onClick={() => navigate('/Home')}>Home</button>
+          </div>
+        )}
 
         <div className="navbar-links">
           <p>NoVegRapix Operational Admin</p>
@@ -42,9 +58,7 @@ const Navbar = () => {
         <div className="navbar-button">
           {isLoggedIn ? (
             <div className="profile-logout-container">
-              <Link to="/profile">
-                <img src={assets.profile} alt="Profile" className="profile-img" />
-              </Link>
+              
               <button className="logout-button" onClick={handleLogout}>
                 Logout
               </button>
@@ -58,7 +72,7 @@ const Navbar = () => {
       </div>
 
       {/* Show banner only when login popup is NOT shown */}
-      {location.pathname === '/' && !showLogin && <OperationalBanner />}
+      {((location.pathname === '/' && !showLogin) || hasLoggedOut) && <OperationalBanner />}
 
       {/* Render login popup overlay */}
       {showLogin && (
