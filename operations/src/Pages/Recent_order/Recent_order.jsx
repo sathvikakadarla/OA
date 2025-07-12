d816f3cdfceb5ff7df25103611a67ba51edb6e14
sathvikakadarla <133248594+sathvikakadarla@users.noreply.github.com>
2025-07-12 16:30:17 +0530

    Update Recent_order.jsx

--- operations/src/Pages/Recent_order/Recent_order.jsx
+++ operations/src/Pages/Recent_order/Recent_order.jsx
@@ -28,28 +28,3 @@ const Recentorders = () => {
 }
 
 export default Recentorders;
-      <div className="order-container">
-      <div className="recent-orders">
-      <h2>Recent Orders</h2>
-      <table className="orders-table">
-        <thead>
-          <tr>
-            <th>Order ID</th>
-            <th>Customer</th>
-            <th>Status</th>
-            <th>Scheduled For</th>
-            <th>Amount</th>
-          </tr>
-        </thead>
-      </table>
-    </div>
-      <div className="delivery-container">
-        <div className="delivery-heading">
-        <h2>Delivery Partner</h2>
-        </div>
-      </div>
-    </div>
-    )
-}
-
-export default Recentorders;
